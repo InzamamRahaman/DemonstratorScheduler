8bb43e01f563ad43aff3c2a451b2be690c928e8b
Keanu Nichols <nichols.keanu9@gmail.com>
2019-02-18 17:44:03 -0400

    Update generate_matrices.py
    Added in rows and columns to the tsv file

--- generate_matrices.py
+++ generate_matrices.py
@@ -3,9 +3,19 @@ import random
 f = open("d_l.tsv","w")
 d_len = 32
 l_len = 64
-parents = 10
-for i in range(parents):
-    for j in range(d_len):
+parents = 1
+rows = 0
+
+f.write("\t")
+for i in range(l_len):
+    if(i == l_len-1):
+        f.write("%d\n"%(i))
+    else:
+        f.write("%d\t"%(i))
+
+for parent in range(parents):
+    for row in range(d_len):
+        f.write("%d\t"%(rows))
         for k in range(l_len):
             num = random.randint(0,1)
             if(k == l_len-1):
@@ -13,3 +23,4 @@ for i in range(parents):
             else:
                 f.write("%d\t"%(num) )
             #print(random.randint(0,1))
+        rows+=1
