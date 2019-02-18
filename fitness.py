1f9126acafe7fb853bea6e47539bf98009e79667
InzamamRahaman <izzyrahaman@gmail.com>
2019-02-18 16:29:43 -0400

    Added the fitness function docs

--- fitness.py
+++ fitness.py
@@ -25,15 +25,19 @@ def fitness(assignments, costs, qualifications, seniority,
                            a demonstrator is qualified to demonstrate for a lab
                            if qualifications[d][l] = 1, then d is qualified for l
                            if qualifications[d][l] = 0, then d is NOT qualified for l
-    :param seniority:
-    :param time_per_lab:
-    :param time_limits:
-    :param demonstrator_requirements:
+    :param seniority: a matrix of size D x L that describes the degree of preference
+                      to assigning a particular demonstrator to a particular lab
+    :param time_per_lab: a vector of size L describing the number of hours
+                         taken by a particular lab
+    :param time_limits: a vector of size D describing the number of hours that
+                        can be worked by a demonstrator in a 2 week cycle
+    :param demonstrator_requirements: a vector of size L describing the number
+                                      of demonstrators required for each lab
     :param qualification_violation_factor:
     :param seniority_bonus_factor:
     :param time_limit_violation_factor:
     :param demonstrator_violation_factor:
-    :return:
+    :return: the cost of an assignment
     """
 
     num_demonstrators, num_labs = assignments.shape
@@ -80,5 +84,49 @@ def fitness(assignments, costs, qualifications, seniority,
     cost_func_value = overall_cost - overall_bonus
 
     return cost_func_value
+
+
+def fitness_func_generator(costs, qualifications, seniority,
+            time_per_lab, time_limits,
+            demonstrator_requirements, qualification_violation_factor,
+            seniority_bonus_factor, time_limit_violation_factor,
+            demonstrator_violation_factor):
+    """
+    Generates a fitness function particular to a set of constants
+    :param costs: a matrix of size L x L that describes the cost of assigning
+                  any arbitrary demonstrator to any two labs.
+                  if costs[l1][l2] = 0, then there is no penalty to co-assignment
+                  if costs[l1][l2] < 0, then we want to ENCOURAGE a demonstrator
+                                        to be assignment to both lab l1 and l2
+                  if costs[l1][l2] > 0, then we want to prevent, as much as possible,
+                                        the assignment of a demonstrator to both
+                                        l1 and l2
+
+    :param qualifications: a binary matrix of size D x L that simply indicates if
+                           a demonstrator is qualified to demonstrate for a lab
+                           if qualifications[d][l] = 1, then d is qualified for l
+                           if qualifications[d][l] = 0, then d is NOT qualified for l
+    :param seniority: a matrix of size D x L that describes the degree of preference
+                      to assigning a particular demonstrator to a particular lab
+    :param time_per_lab: a vector of size L describing the number of hours
+                         taken by a particular lab
+    :param time_limits: a vector of size D describing the number of hours that
+                        can be worked by a demonstrator in a 2 week cycle
+    :param demonstrator_requirements: a vector of size L describing the number
+                                      of demonstrators required for each lab
+    :param qualification_violation_factor:
+    :param seniority_bonus_factor:
+    :param time_limit_violation_factor:
+    :param demonstrator_violation_factor:
+    :return function that accepts a matrix of assignments to compute the fitness
+    """
+    def f(assignment):
+        cost = fitness(assignment, costs, qualifications, seniority,
+            time_per_lab, time_limits,
+            demonstrator_requirements, qualification_violation_factor,
+            seniority_bonus_factor, time_limit_violation_factor,
+            demonstrator_violation_factor)
+        return cost
+    return f
 
 
