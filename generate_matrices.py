a8522cd7982569ac2bab3556f669436d8cf7ed50
Keanu Nichols <nichols.keanu9@gmail.com>
2019-02-18 17:50:09 -0400

    Remove rows
    Took out the rows which was just showing the row number.

--- generate_matrices.py
+++ generate_matrices.py
@@ -4,23 +4,19 @@ f = open("d_l.tsv","w")
 d_len = 32
 l_len = 64
 parents = 1
-rows = 0
 
 f.write("\t")
-for i in range(l_len):
-    if(i == l_len-1):
+for i in range(l_len-1):
+    if(i == l_len-2):
         f.write("%d\n"%(i))
     else:
         f.write("%d\t"%(i))
 
 for parent in range(parents):
     for row in range(d_len):
-        f.write("%d\t"%(rows))
         for k in range(l_len):
             num = random.randint(0,1)
             if(k == l_len-1):
                 f.write("%d\n"%(num) )
             else:
                 f.write("%d\t"%(num) )
-            #print(random.randint(0,1))
-        rows+=1
