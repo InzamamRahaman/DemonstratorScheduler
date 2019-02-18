5011a97ff383cddc02a70355e1075312dbe147e1
Keanu Nichols <nichols.keanu9@gmail.com>
2019-02-18 18:15:54 -0400

    Multiple Parents
    Works for multiple parents.

--- generate_matrices.py
+++ generate_matrices.py
@@ -3,11 +3,10 @@ import random
 f = open("d_l.tsv","w")
 d_len = 32
 l_len = 64
-parents = 1
+parents = 2
 
-f.write("\t")
-for i in range(l_len-1):
-    if(i == l_len-2):
+for i in range(l_len):
+    if(i == l_len-1):
         f.write("%d\n"%(i))
     else:
         f.write("%d\t"%(i))
