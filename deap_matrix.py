9f2fab01d28a6f5c907108616a4c814ffdf1ca5c
Keanu Nichols <nichols.keanu9@gmail.com>
2019-02-19 18:13:02 -0400

    Updated evalOneMax
    Looking at going through the arrays and determining the number of one's in each array.

--- deap_matrix.py
+++ deap_matrix.py
@@ -20,8 +20,41 @@ toolbox.register("individual", init2d, creator.individual,
 low=0,high=2,shape=(32,64))
 toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 
-pop = toolbox.population(n=2)
-print(len(pop))
-print(len(pop[0]))
-print(type(pop))
-print(np.shape(pop))
+#pop = toolbox.population(n=1)
+
+
+def evalOneMax(individual):
+    print("Here")
+    li = []
+    for i in individual:
+        sum = 0
+        for j in i:
+            if(j == 1):
+                sum+=1
+        li.append(sum)
+
+        print(i,"\n")
+    #print(individual,"\n")
+    return li
+
+toolbox.register("evaluate", evalOneMax)
+
+
+def main():
+    pop = toolbox.population(n=1)
+    print(pop)
+    print(len(pop))
+    print(len(pop[0]))
+    print(type(pop))
+    print(np.shape(pop))
+    print(pop[0][0])
+    fitnesses = list(map(toolbox.evaluate, pop))
+    for ind, fit in zip(pop, fitnesses):
+        ind.fitness.values = fit
+
+    print(fitnesses)
+    #print(len(fitnesses))
+    #print(len(fitnesses[0]))
+
+if __name__ == "__main__":
+    main()
