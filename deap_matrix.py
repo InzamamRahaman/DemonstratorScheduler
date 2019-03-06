ff4a6dfe3531cb4a0f3cae7aaef8a0a235c51708
Keanu Nichols <nichols.keanu9@gmail.com>
2019-03-06 17:29:39 -0400

    Generating populations
    I believe we now have new generations being developed, the next step would be to take out the elites.

--- deap_matrix.py
+++ deap_matrix.py
@@ -1,3 +1,8 @@
+'''
+Call function instead when using the fitness.py update code to suit
+
+'''
+
 import numpy as np
 from deap import base
 from deap import creator
@@ -27,17 +32,26 @@ toolbox.register("population", tools.initRepeat, list, toolbox.individual)
 def evalOneMax(individual,items):
     costs = []
     print("Inside")
-    print(len(individual))
+    #print(len(individual))
     for i in range(len(individual)):
-        costs.append(fitness.fitness(individual[i],items[0],items[1],items[2],\
-        items[3],items[4],items[5],0,0,0,0))
+        costs.append( tuple([fitness.fitness(individual[i],items[0],items[1],items[2],\
+        items[3],items[4],items[5],0,0,0,0)]) )
     return(costs)
+
+def mutate(individual):
+    row = random.randint(0,individual.shape[0]-1)
+    col = random.randint(0,individual.shape[1]-1)
+    #print(individual[row][col])
+    individual[row][col] = 1 - (individual[row][col]) 
+    #print("Here ", individual[row][col])
+    #return row,col
+
 
 
 #Genetic Operators
 toolbox.register("evaluate", evalOneMax)
 toolbox.register("mate", tools.cxTwoPoint)
-toolbox.register("mutate", tools.mutFlipBit, indpb=0.05)
+toolbox.register("mutate", mutate)
 
 '''Not sure if I'm doing this correctly
 in the example it seemed like they just copied the 
@@ -60,7 +74,7 @@ def main():
     costs = np.random.randint(-20,20,size=(L,L))
     qualifications = np.random.randint(2,size=(D,L) )
     pop = toolbox.population(n=10)
-    print(type(pop[0]))
+    #print(type(pop[0]))
     seniority = np.random.randint(5,size=(D,L))
     time_per_lab = np.random.randint(5,size=(L,1))
     time_limits = np.random.randint(10,size=(D,1))
@@ -72,42 +86,96 @@ def main():
     
     print("after")
     #fitnesses = list(map(toolbox.evaluate,[pop,items]))
-    fitnesses = toolbox.evaluate(pop,items)
+    fitnesses = toolbox.evaluate(pop,items=items)
     #fitnesses = list(map(fitnesses,pop))
     #fitnesses = (map(fitnesses, pop))
     #fitnesses = list(zip(fitnesses))
-    print(fitnesses)
-    print(max(fitnesses))
+    #print(list(fitnesses))
+    print((fitnesses))
 
     for ind, fit in zip(pop, fitnesses):
         ind.fitness.values = fit
     #map(toolbox.evaluate,pop)
 
-    offspring = toolbox.select(pop,len(pop))
-    print("Offspring lenght: " + str(len(offspring)) )
-    print(len(pop))
+    fits = [ind.fitness.values[0] for ind in pop]
 
-    i = 0
+    #print(fits)
+    #print(pop[0].fitness)
 
-    for child1,child2 in zip(offspring[::2], offspring[1::2]):
-        if(random.random() < CXPB):
-            #print(child1)
-            #print(child2)
-            i+=1
-    print("Number of children: ",i)
-    #Evaluation the entire population
-    '''
-    for ind, fit in zip(pop, fitnesses):
-        ind.fitness.values = fit
+    g = 0
+
+    while(max(fits) < 100 and g < 50):
+        g+=1
+        print("-- Generation %i -- " %g)
+
+        offspring = toolbox.select(pop,len(pop))
+        offspring = list(map(toolbox.clone,offspring))
+
+        i = 0
+
+        for child1,child2 in zip(offspring[::2], offspring[1::2]):
+            if(random.random() < CXPB):
+                #print(child1)
+                #print(child2)
+                i+=1
+                #print("It's here")
+                del child1.fitness.values
+                del child2.fitness.values
+
+        for mutant in offspring:
+            if random.random() < MUTPB:
+                '''
+                print(type(mutant))
+                print(mutant)
+                print(mutant[0][0])
+                '''
+                '''
+                if(mutant[0][0]):
+                    mutant[0][0] = 0
+                else:
+                    mutant[0][0] = 1
+                '''
+                #print(mutant[0][0])
+                #print(mutant.shape)
+                #print(mutant.shape[0])
+                #row,col = toolbox.mutate(mutant)
+                toolbox.mutate(mutant)
+                #print(mutant[row][col])
+                del mutant.fitness.values
+
+        invalid_ind = [ind for ind in offspring if not ind.fitness.valid]
+        fitnesses = toolbox.evaluate(invalid_ind,items=items)
+
+        for ind, fit in zip(invalid_ind, fitnesses):
+            ind.fitness.values = fit
+
+        pop[:] = offspring
+        fits = [ind.fitness.values[0] for ind in pop]
+        
+
+        length = len(pop)
+        mean = sum(fits) / length
+        sum2 = sum(x*x for x in fits)
+        std = abs(sum2/length - mean**2)**0.5
+        print(fits)
+
+        print("  Min %s" % min(fits))
+        print("  Max %s" % max(fits))
+        print("  Avg %s" % mean)
+        print("  Std %s" % std)
+        #Evaluation the entire population
+        '''
+        for ind, fit in zip(pop, fitnesses):
+            ind.fitness.values = fit
 
 
-    fits = [ind.fitness.values for ind in pop]
-    print(max(fitnesses))
-    print(fitnesses)
-    print(fits)
-    #print(len(fitnesses))
-    #print(len(fitnesses[0]))
-    '''
+        fits = [ind.fitness.values for ind in pop]
+        print(max(fitnesses))
+        print(fitnesses)
+        print(fits)
+        #print(len(fitnesses))
+        #print(len(fitnesses[0]))
+        '''
 
 if __name__ == "__main__":
     main()
